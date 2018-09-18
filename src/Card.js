06eb07eb210c02875c2535e8de70b349ac6983d2
Chih Ying <rockids@hotmail.com.tw>
2018-09-18 15:19:47 +0800

    feat: Style the card

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img src="https://robohash.org/test/set_set3?size=200x200" alt="robot"/>
       <div>
         <h2>Name</h2>
