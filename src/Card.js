5dcbc82b01b71cbfcebfd1e4803b0c9630b82d88
Chih Ying <rockids@hotmail.com.tw>
2018-09-18 15:10:52 +0800

    feat: Create Card.js

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt="robot"/>
+      <div>
+        <h2>Name</h2>
+        <p>Name@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
