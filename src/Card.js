b89d180f44a1e4bcac62a6b16c44d6d641c2aed4
Chih Ying <rockids@hotmail.com.tw>
2018-09-18 15:16:03 +0800

    feat: Use robohash api for img

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
   return (
     <div>
-      <img src="" alt="robot"/>
+      <img src="https://robohash.org/test/set_set3?size=200x200" alt="robot"/>
       <div>
         <h2>Name</h2>
         <p>Name@gmail.com</p>
