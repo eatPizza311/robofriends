b030a042d073e1b94a2235d06679e6c27f3f50ce
Chih Ying <rockids@hotmail.com.tw>
2018-09-18 15:34:05 +0800

    feat: Add more card by using data from robots.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ id, name, email }) => { //Destructuring in parameter
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src="https://robohash.org/test/set_set3?size=200x200" alt="robot"/>
+      <img src={`https://robohash.org/${id}/set_set3?size=200x200`} alt="robot"/>
       <div>
-        <h2>Name</h2>
-        <p>Name@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
