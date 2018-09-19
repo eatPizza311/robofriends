38275ffb585e7ff0381bdf78436d5cf5e6541bd8
Chih Ying <rockids@hotmail.com.tw>
2018-09-19 10:57:48 +0800

    feat: Add heading "RoboFriends"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { robots } from './robots';
 
 const App = () => {
   return(
-    <CardList robots={robots}/>
+    <div>
+      <h1>RoboFriends</h1>
+      <CardList robots={robots}/>
+    </div>
   );
 }
 
