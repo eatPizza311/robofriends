be99ca08886cdf2f4fb93f57281f597fe3e356ea
Chih Ying <rockids@hotmail.com.tw>
2018-09-20 10:49:33 +0800

    feat: Add loading message

--- src/App.js
+++ src/App.js
@@ -26,14 +26,17 @@ componentDidMount() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if ( this.state.robots.length === 0 ) {
+      return <h1 className="tc">Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
